3910c66b00e3727b9cb1d98b829738275d4e5673
E Salmon <erinsalmon@protonmail.com>
2022-02-19 23:33:51 +0100

    download button and link to Github fixed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ import GitHub from "../images/GitHub.png"
 export default function Footer() {
     return (
         <div className="Footer">
-            <a href="https://www.github.com/salmoner6">
+            <a target="_blank" href="https://www.github.com/salmoner6">
                 <img src={GitHub}></img>
             </a>
         </div>
--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Headshot from "../images/Headshot.jpg"
-
-
+import Pdf from "./CV_ErinSalmon.pdf"
 
 export default function Info() {
     return (
@@ -11,7 +10,7 @@ export default function Info() {
             <h3 className="Job">Masters Student</h3>
             <div className="Buttons">
                 <a href="mailto:erinsalmon@protonmail.com" className="Email"><i className="fa fa-envelope"></i>Email</a>
-                <a href="CV_ErinSalmon.pdf" download className="CV"><i className="fa fa-download"></i>CV</a> 
+                <a href={Pdf} download className="CV"><i className="fa fa-download"></i>CV</a> 
             </div>
 
 
