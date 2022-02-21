795ffd8204b5e7764ced2065c2c418eb5986ad3f
E Salmon <erinsalmon6@gmail.com>
2022-02-21 15:11:34 +0100

    add Age component to info section

---
+++ src/Components/Age.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Age = ({age}) => {
+    return (
+            <h2 className="Age">{age} ans</h2>
+    )
+}
+
+export default Age;
--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,18 +1,19 @@
 import React from "react"
 import Headshot from "../images/Headshot.jpg"
 import Pdf from "./CV_ErinSalmon.pdf"
+import Age from "./Age"
 
 export default function Info() {
     return (
         <div className="Info">
             <img src={Headshot}></img>
             <h1 className="Info--Name">Erin Salmon</h1>
+            <Age age={28}/>
             <h3 className="Info--Job">Masters Student</h3>
             <div className="Info--Buttons">
                 <a href="mailto:erinsalmon@protonmail.com" className="Email"><i className="fa fa-envelope"></i>Email</a>
                 <a href={Pdf} target="_blank" className="CV"><i className="fa fa-download"></i>CV</a> 
             </div>
-
 
         </div>
     )
